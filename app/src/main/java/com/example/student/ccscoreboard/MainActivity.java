d313fe2ebf8e6cd832fb23e181796d7c3cc99dd2
Ricardo Harris <ricardoharris@ymail.com>
2016-11-14 02:43:24 -0800

    4th commit, broken milestone 3
    Dissappointed in myself. I thought I knew Java, I was wrong. I tap. I
    don't have any future advise for myself.

--- app/src/main/java/com/example/student/ccscoreboard/MainActivity.java
+++ app/src/main/java/com/example/student/ccscoreboard/MainActivity.java
@@ -4,15 +4,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView t1butt;
+    viewVar.setText(""+0));
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //look in resources folder for layout and in layout folder for activity.main
-
+        t1butt = (TextView)findViewById(R.id.t1btv);
+        t1btv.setText("0");
+    }
+    public void t1p1(){
+        1++;
     }
 }
+//I tap out. I thought I knew java.. I was wrong
 
 //R. refers to our assets and resources (sound files/images/xml files)
 //when R is a big red error, the XML file has an error
